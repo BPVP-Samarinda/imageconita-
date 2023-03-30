18e9ba58173b17f3f08c87c81ebcb4d24503fb01
imageconita <image19.fc@gmail.com>
2023-03-30 10:59:53 +0800

    init project

---
+++ app/Filament/Resources/ProdukResource.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProdukResource\Pages;
+use App\Filament\Resources\ProdukResource\RelationManagers;
+use App\Models\Produk;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProdukResource extends Resource
+{
+    protected static ?string $model = Produk::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama'),
+                Forms\Components\TextArea::make('deskripsi'),
+                Forms\Components\select::make('kategori_id')->relationship('kategori','nama'),
+                Forms\Components\TextInput::make('harga'),
+                Forms\Components\TextInput::make('stok'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama'),
+                Tables\Columns\TextColumn::make('kategori.nama'),
+                Tables\Columns\TextColumn::make('harga'),
+                Tables\Columns\TextColumn::make('stok'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageProduks::route('/'),
+        ];
+    }    
+}
